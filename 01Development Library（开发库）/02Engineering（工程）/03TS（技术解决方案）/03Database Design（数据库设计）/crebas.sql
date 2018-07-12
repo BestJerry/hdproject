44627ca57cca737c4fd2a660aa6a80fb7b45369b
rrrrrng <845119166@qq.com>
2018-07-12 15:01:46 +0800

    修改了数据库设计

--- 01Development Library（开发库）/02Engineering（工程）/03TS（技术解决方案）/03Database Design（数据库设计）/crebas.sql
+++ 01Development Library（开发库）/02Engineering（工程）/03TS（技术解决方案）/03Database Design（数据库设计）/crebas.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     18/7/12 9:51:09                              */
+/* Created on:     18/7/12 14:51:27                             */
 /*==============================================================*/
 
 
@@ -21,13 +21,13 @@ drop table if exists power;
 /*==============================================================*/
 create table account
 (
-   acc_id               int not null auto_increment,
+   acc_id               int not null,
    account_type         int not null comment '数据范围：1~4
             1：管理员
             2：前台
             3：客户经理
             4：经理',
-   bus_id               int,
+   bus_id               int not null,
    acc_name             varchar(50) not null,
    acc_psd              varchar(50) not null,
    isStart              bool not null,
@@ -40,16 +40,16 @@ create table account
 /*==============================================================*/
 create table business
 (
-   bus_id               int not null auto_increment,
+   bus_id               int not null,
    con_id               int not null,
-   bus_type             varchar(50),
-   bus_name             varchar(50),
-   bus_add              varchar(50),
-   bus_phone            varchar(50),
+   bus_type             varchar(50) not null,
+   bus_name             varchar(50) not null,
+   bus_add              varchar(50) not null,
+   bus_phone            varchar(50) not null,
    bus_postcode         numeric(10,0),
-   bus_star             int comment '数据范围：0~5  表示0到5星级',
-   con_intergral        decimal,
-   discount             decimal,
+   bus_star             int not null comment '数据范围：0~5  表示0到5星级',
+   con_intergral        decimal not null,
+   discount             decimal not null,
    primary key (bus_id)
 );
 
@@ -58,14 +58,14 @@ create table business
 /*==============================================================*/
 create table contacts
 (
-   con_id               int not null auto_increment,
-   con_title            varchar(50),
-   con_name             varchar(50),
-   con_position         varchar(50),
+   con_id               int not null,
+   con_title            enum('先生','女士') not null,
+   con_name             varchar(50) not null,
+   con_position         varchar(50) not null,
    con_tel              varchar(50),
    con_mobile           numeric(20,0),
    con_fax              numeric(20,0),
-   con_email            varchar(30),
+   con_email            varchar(30) not null,
    primary key (con_id)
 );
 
@@ -74,10 +74,10 @@ create table contacts
 /*==============================================================*/
 create table log
 (
-   log_id               int not null auto_increment,
-   acc_id               int,
-   log_type             bool comment 'true为登陆，false为退出',
-   log_time             datetime,
+   log_id               int not null,
+   acc_id               int not null,
+   log_type             bool not null comment 'true为登陆，false为退出',
+   log_time             datetime not null,
    primary key (log_id)
 );
 
@@ -86,10 +86,10 @@ create table log
 /*==============================================================*/
 create table operation
 (
-   opt_id               int not null auto_increment,
-   acc_id               int,
+   opt_id               int not null,
+   acc_id               int not null,
    opt_date             datetime not null,
-   opt_url              varchar(50),
+   opt_url              varchar(50) not null,
    opt_fun              varchar(50),
    opt_exp              varchar(50),
    remarks              varchar(50),
@@ -106,21 +106,21 @@ create table power
             2：前台
             3：客户经理
             4：经理',
-   record               bool comment 'true:只可修改30分钟内记录，且只能修改自己账户添加的接待记录
+   record               bool not null comment 'true:只可修改30分钟内记录，且只能修改自己账户添加的接待记录
             false:可以修改本店48小时内的接待记录',
-   information          bool comment 'true:只可以修改30分钟内记录，且只能修改自己账户添加的会员资料
+   information          bool not null comment 'true:只可以修改30分钟内记录，且只能修改自己账户添加的会员资料
             false:任何时间均可修改本店的会员信息',
-   business             bool comment 'true:可以
+   business             bool not null comment 'true:可以
             false:不可以',
-   report               bool comment 'true:可以
+   report               bool not null comment 'true:可以
             false:不可以',
-   account              bool comment 'true:可以
+   account              bool not null comment 'true:可以
             false:不可以',
-   cardtype             bool comment 'true:可以
+   cardtype             bool not null comment 'true:可以
             false:不可以',
-   recharge             bool comment 'true:可以
+   recharge             bool not null comment 'true:可以
             false:不可以',
-   manager              bool comment 'true:可以
+   manager              bool not null comment 'true:可以
             false:不可以',
    primary key (account_type)
 );
