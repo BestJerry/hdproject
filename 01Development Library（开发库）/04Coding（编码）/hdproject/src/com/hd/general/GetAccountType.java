e0a5b4202a37df91ff627ce8158474f6ddb436fe
rrrrrng <845119166@qq.com>
2018-07-15 22:29:23 +0800

    新增查询当前登录用户

---
+++ 01Development Library（开发库）/04Coding（编码）/hdproject/src/com/hd/general/GetAccountType.java
@@ -0,0 +1,75 @@
+package com.hd.general;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import com.hd.beans.Account;
+import com.hd.tools.Response;
+
+@WebServlet("/getAccountType")
+public class GetAccountType extends HttpServlet {
+
+	/**
+		 * Constructor of the object.
+		 */
+	public GetAccountType() {
+		super();
+	}
+
+	/**
+		 * Destruction of the servlet. <br>
+		 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+		 * The doGet method of the servlet. <br>
+		 *
+		 * This method is called when a form has its tag value method equals to get.
+		 * 
+		 * @param request the request send by the client to the server
+		 * @param response the response send by the server to the client
+		 * @throws ServletException if an error occurred
+		 * @throws IOException if an error occurred
+		 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Account account = (Account)request.getSession().getAttribute("account");
+		JSONObject jsonObject = new JSONObject(new Response(account,"0",""));
+		response.getWriter().print(jsonObject);
+	}
+
+	/**
+		 * The doPost method of the servlet. <br>
+		 *
+		 * This method is called when a form has its tag value method equals to post.
+		 * 
+		 * @param request the request send by the client to the server
+		 * @param response the response send by the server to the client
+		 * @throws ServletException if an error occurred
+		 * @throws IOException if an error occurred
+		 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	/**
+		 * Initialization of the servlet. <br>
+		 *
+		 * @throws ServletException if an error occurs
+		 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
